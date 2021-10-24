fdc7a7d93717cedbdea72de054386a3cc5cad286
Raj <pandeyg.raj@gmail.com>
2021-10-24 04:01:06 +0000

    object size increased

--- src/zk_client.cpp
+++ src/zk_client.cpp
@@ -102,9 +102,9 @@ void thread_function(int i)
     SET_c[i] = 0;
     struct Stat stat;
     int rc = 0;
-    char buffer[512] = {0};
+    char buffer[1024] = {0};
     int buflen = sizeof(buffer);
-    const char *buffer_set = "rajudatanew";
+    const char *buffer_set = "What about this random String ? You are sure, right? But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?";
     int buf_set_len = strlen(buffer_set);
 
     const char* path = "/raju";
