b0f14f7728982920ae0c957ca2ede01c93d1071d
Raj <pandeyg.raj@gmail.com>
2021-10-24 03:24:27 +0000

    minor update

--- src/zk_client.cpp
+++ src/zk_client.cpp
@@ -11,7 +11,10 @@ using std::endl;
 
 static int NOOFTHREAD = 10 ;
 static int experiment_time = 60;
-
+static double checkdURATION[256] = {0};
+static double durations[256] = {0.0};
+static double getdurations[256] = {0.0};
+static double setdurations[256] = {0.0};
 extern "C"{
 
 ZOOAPI int zoo_get(zhandle_t *zh, const char *path, int watch, char *buffer, int* buffer_len, struct Stat *stat);
@@ -29,12 +32,17 @@ void thread_function(int );
 static int connected = 0;
 static int expired = 0;
 
-static int GET_c[64] = {0};
-static int SET_c[64] = {0};
-static int TOTAL[64] = {0};
+static int GET_c[256] = {0};
+static int SET_c[256] = {0};
+static int TOTAL[256] = {0};
+
 zhandle_t * zk = 0;
 int main (int argc, char** argv)
 {
+    if(argc!=3){
+        cout<<"usage: ./a.out {time} {#thread}"<<endl;
+        return -1;
+    }
     experiment_time = atoi(argv[1]);
     NOOFTHREAD = atoi(argv[2]);
     zk =  zookeeper_init("34.94.181.64:2181",main_watcher,15000, 0,0,0);
@@ -60,23 +68,38 @@ int main (int argc, char** argv)
 
     zookeeper_close(zk);
     int totalGet = 0,totalSet = 0 ,total = 0;
+    double checkRaj= 0.0,totalLatency = 0.0,getLatency = 0.0,setLatency = 0.0;
     for(int i =0;i<NOOFTHREAD;i++)
     {
-        totalGet+= GET_c[i];
-        totalSet+= SET_c[i];
+        durations[i] = getdurations[i] +  setdurations[i];
+
+        totalGet += GET_c[i];
+        totalSet += SET_c[i];
+
         total+= TOTAL[i];
+        
+        getLatency += getdurations[i];
+        setLatency += setdurations[i];
+        totalLatency += checkdURATION[i];
+        checkRaj +=  durations[i];
     }
 
     cout<<"TIME of Experiment "<<experiment_time<<endl<<"THREAD: "<<NOOFTHREAD<<endl;
     cout<<"TOTAL: "<<total<<" GET: "<<totalGet<<" SET: "<<totalSet<<endl;
     cout<<"Read Ratio: "<<((float)totalGet/total)*100<<endl;
-    cout<<"rate: "<<(float)total/experiment_time;
+    cout<<"rate: "<<(float)total/experiment_time<<endl;
+    //cout<<"avg latency (total)(ms) "<< (totalLatency*0.001)/total<<endl;
+    //cout<<"avg checkRaj (total)(ms) "<< (checkRaj*0.001)/total<<endl;
+    cout<<"avg GET latency (ms) "<< (getLatency*0.001)/totalGet<<endl;
+    cout<<"avg SET latency (ms) "<< (setLatency*0.001)/totalSet<<endl;
     return 0;
 }
 
 void thread_function(int i)
 {
     TOTAL[i] = 0;
+    GET_c[i] = 0;
+    SET_c[i] = 0;
     struct Stat stat;
     int rc = 0;
     char buffer[512] = {0};
@@ -86,8 +109,9 @@ void thread_function(int i)
 
     const char* path = "/raju";
     char filename[64] = {0};
-    sprintf(filename, "file%d.txt",i);
+    sprintf(filename, "results/file%d.txt",i);
     FILE* fptr = fopen(filename,"w");
+    fprintf(fptr,"thread,start,end,diff,data\n");
     auto start_point = time_point_cast<milliseconds>(system_clock::now());
     auto end_point = start_point + seconds(experiment_time);
     time_point <system_clock, milliseconds> tp = time_point_cast<milliseconds>(system_clock::now());
@@ -128,8 +152,10 @@ int my_zoo_set(zhandle_t *zk, const char *path, const char *buffer,
             fprintf(fptr,"my_zoo_create error   %d \n",rc);
     }
     auto m_End = time_point_cast<std::chrono::microseconds>(std::chrono::system_clock::now()).time_since_epoch().count();
-    fprintf(fptr,"SET: No error thread# %d,   start: %ld,   end: %ld,   diff: %ld, data%s\n",i,m_Start,m_End,m_End-m_Start,buffer);
-        
+    setdurations[i]+= m_End-m_Start;
+    checkdURATION[i]+= m_End-m_Start;
+    //fprintf(fptr,"SET: No error thread# %d,   start: %ld,   end: %ld,   diff: %ld, data%s\n",i,m_Start,m_End,m_End-m_Start,buffer);
+      fprintf(fptr,"SET#%d,%ld,%ld,%ld,%s\n",i,m_Start,m_End,m_End-m_Start,buffer);  
     return rc;
 }
 
@@ -146,7 +172,10 @@ int my_zoo_get(zhandle_t *zk, const char *path, int watch, char *buffer,
         auto m_End = time_point_cast<std::chrono::microseconds>(std::chrono::system_clock::now()).time_since_epoch().count();
         if(rc==0) 
         {
-            fprintf(fptr,"GET: No error thread# %d,   start: %ld,   end: %ld,   diff: %ld, data%s\n",i,m_Start,m_End,m_End-m_Start,buffer);
+            //fprintf(fptr,"GET: No error thread# %d,   start: %ld,   end: %ld,   diff: %ld, data%s\n",i,m_Start,m_End,m_End-m_Start,buffer);
+            getdurations[i]+= m_End-m_Start;
+            checkdURATION[i]+= m_End-m_Start;
+            fprintf(fptr,"GET#%d,%ld,%ld,%ld,%s\n",i,m_Start,m_End,m_End-m_Start,buffer); 
         }
         else 
             fprintf(fptr,"my_zoo_get error   %d \n",rc);
